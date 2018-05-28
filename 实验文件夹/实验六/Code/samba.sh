4d23ce5f8a8e8bf8df658fa7814eafc030c7d755
CUCfromHY001 <caojiahe009@gmail.com>
2018-05-28 19:37:41 +0800

    提交第七次实验截图和代码

--- 实验文件夹/实验六/Code/samba.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo apt-get update
-sudo apt-get -y install samba samba-common
-service iptables stop
-sudo mkdir /home/share
-sudo chmod 777 /home/share
-sudo useradd smbuse
-sudo smbpasswd -a smbuser
-sudo service smbd restart
